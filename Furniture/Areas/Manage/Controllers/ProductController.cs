072ddf8fd84a1abd0914ba76aeb8d707060075fc
Khalig <khaligfm@code.edu.az>
2023-09-03 03:58:51 +0400

    Crud-Product

---
+++ Furniture/Areas/Manage/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Furniture.DAL;
+using Furniture.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Furniture.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class ProductController : Controller
+    {
+        private readonly FurnutireContext _context;
+
+        public ProductController(FurnutireContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Product> products = _context.Products.Include(x=>x.Category).Include(i=>i.Images).ToList();
+            return View(products);
+        }
+    }
+}
