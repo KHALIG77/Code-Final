f2358226fc78b66d7a0f5f7886661ddb8df31b33
Khalig <khaligfm@code.edu.az>
2023-09-09 16:03:33 +0400

    Admin-login

---
+++ Furniture/Areas/Manage/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Furniture.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace Furniture.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager,RoleManager<IdentityRole> roleManager)
+        {
+          _userManager = userManager;
+           _signInManager = signInManager;
+           _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> CreateAdmin()
+        {
+            AppUser admin = new AppUser
+            {
+                UserName = "xaliq",
+                IsAdmin = true,
+            };
+
+            var result = await _userManager.CreateAsync(admin, "xaliq123");
+
+            await _userManager.AddToRoleAsync(admin, "Admin");
+            return Json(result);
+
+        }
+        public async Task<IActionResult> AddRole()
+        {
+            AppUser admin = _userManager.Users.FirstOrDefault();
+           var result =   await  _userManager.AddToRoleAsync(admin, "Admin");
+            return Content(result.ToString());
+        }
+
+        public async Task<ActionResult> CreateRole()
+        {
+            await _roleManager.CreateAsync(new IdentityRole("SuperAdmin"));
+            await _roleManager.CreateAsync(new IdentityRole("Admin"));
+            await _roleManager.CreateAsync(new IdentityRole("Member"));
+
+            return Content("Correct");
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
--- Furniture/Program.cs
+++ Furniture/Program.cs
@@ -26,7 +26,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -34,7 +33,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllerRoute(
 	name: "areas",
